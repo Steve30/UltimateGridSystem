af0ba72f7dfe876b0d2fea9f77aff6736d93f2d7
Juhász István <juhasz.istvan28@windowslive.com>
2018-02-26 16:52:15 +0100

    Create structrures

---
+++ src/UltimateGridSystem.js
@@ -0,0 +1,5 @@
+import { TableContainer } from "./components/TableContainer.js";
+
+"use strict";
+
+const tableContainer = new TableContainer();
---
+++ src/components/TableContainer.js
@@ -0,0 +1,9 @@
+"use strict";
+
+export class TableContainer {
+  constructor() {
+    this.container = document.querySelector("#tableContainer");
+
+    console.dir(this.container);
+  }
+}
