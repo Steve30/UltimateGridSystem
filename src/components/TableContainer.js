6c47784e7d433d17cda91021edcc6d091407668f
Juhász István <juhasz.istvan28@windowslive.com>
2018-03-10 13:13:33 +0100

    Use proxy

--- src/components/TableContainer.js
+++ src/components/TableContainer.js
@@ -2,8 +2,25 @@
 
 export class TableContainer {
   constructor() {
-    this.container = document.querySelector("#tableContainer");
+    this.container = document.querySelector("#grid-template");
+    const clone = document.importNode(this.container.content, true);
 
-    console.dir(this.container);
+    document.body.appendChild(clone);
+
+    this.createProxyTable();
+
+    this.proxy.innerHTML = "<div>Valami</div>";
+    this.proxy.innerHTML = "<div>Valami2</div>";
+  }
+
+  createProxyTable() {
+    this.proxy = new Proxy(this.container, {
+      set(target, property, value) {
+        const prevValue = target[property];
+
+        //document.body[property] = `${prevValue}${value}`;
+        return Reflect.set(target, property, value);
+      }
+    })
   }
 }
