11e465644190de284954eb0826c932f227e9d508
Juhász István <juhasz.istvan28@windowslive.com>
2018-04-09 16:17:30 +0200

    Begin context menu implement

--- src/js/UltimateGridSystem.js
+++ src/js/UltimateGridSystem.js
@@ -1,5 +1,6 @@
 import { TableContainer } from "./components/TableContainer.js";
 import { default as GridEvent } from "./events/gridEvent.js";
+import ContextMenu from "./components/ContextMenu.js";
 
 const tableContainer = new TableContainer({
   isSearchRow: true,
@@ -12,8 +13,17 @@ const gridEvent = new GridEvent({
   deleteRow: true
 });
 
+const contextMenu = new ContextMenu();
+
 tableContainer.renderGridLayout()
   .then(() => {
     tableContainer.initGridTable();
     gridEvent.initEvent(tableContainer.gridContainer);
+
+    tableContainer.gridContainer.addEventListener("contextmenu", (event) => {
+      event.preventDefault();
+
+      const { clientX, clientY } = event;
+      contextMenu.render(clientX, clientY);
+    });
   })
---
+++ src/js/components/ContextMenu.js
@@ -0,0 +1,9 @@
+export default class ContextMenu {
+  constructor() {
+
+  }
+
+  render(xPosition, yPosition) {
+
+  }
+}
