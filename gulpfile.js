6c3e74822336a919bc4ed2fe6e2ea8655af886db
István Juhász <juhasz.istvan28@windowslive.com>
2018-06-02 14:14:06 +0200

    Modify eslint and gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,31 @@
 const gulp        = require('gulp');
 const browserSync = require('browser-sync').create();
+const guppy = require("git-guppy")(gulp);
+const gulpEslint = require("gulp-eslint");
+const gulpFilter = require("gulp-filter");
 
-gulp.task('css', function() {
+gulp.task('css', () => {
   return gulp.src("./src/style/**/*.css")
     .pipe(browserSync.stream());
 });
 
+gulp.task('lint', () => {
+    return gulp.src('./src/js/**/*.js')
+      .pipe(gulpEslint())
+      .pipe(gulpEslint.format())
+      .pipe(gulpEslint.failAfterError());
+});
+
+gulp.task('pre-commit', () => {
+  return gulp.src(guppy.src("pre-commit"))
+    .pipe(gulpFilter(["src/js/**/*.js"]))
+    .pipe(gulpEslint())
+    .pipe(gulpEslint.format())
+    .pipe(gulpEslint.failAfterError());
+});
+
 // Static server
-gulp.task('browser-sync', function() {
+gulp.task('browser-sync', ["pre-commit"], () => {
     browserSync.init({
         server: {
             baseDir: "./"
